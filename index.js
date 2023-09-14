9f4e2f90473e96c55245ec56720fc88574fdd133
SanadKhan <khansanad72485@gmail.com>
2023-09-14 18:34:34 +0530

    check deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({"msg": 'Running from Backend!'})
+  res.send({"msg": 'Running from Backend!!'})
 });
 
 app.listen(4000, () => {
